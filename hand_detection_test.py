591c8c51b5a385c439b2ba65dbcbf2fd74df7e2c
KiranEaswar <111830714+KiranEaswar@users.noreply.github.com>
2025-01-18 09:05:42 +0530

    Update hand_detection_test.py

--- hand_detection_test.py
+++ hand_detection_test.py
@@ -1,30 +1,24 @@
-# Import the necessary packages
 from flask import Flask, Response
 import cv2
 
-# Initialize the Flask application
 app = Flask(__name__)
-
-# Initialize the video capture object
-camera = cv2.VideoCapture(0)  # Use 0 for the default camera
-
-def generate_frames():
-    while True:
-        # Read the current frame from the camera
-        success, frame = camera.read()
-        if not success:
-            break
-        else:
-            # Encode the frame in JPEG format
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            # Yield the output frame in the byte format
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
-    # Return the response generated along with the specific media type
+    camera = cv2.VideoCapture(0)  # Use 0 for the default camera
+    def generate_frames():
+        while True:
+            success, frame = camera.read()
+            if not success:
+                break
+            else:
+                height,width,_ = frame.shape
+                midline = width//2
+                cv2.line(frame,(width,0),(width,height),color=(255,0,0),thickness=1.5)
+                ret, buffer = cv2.imencode('.jpg', frame)
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/')
@@ -32,4 +26,4 @@ def index():
     return "<h1>Video Feed</h1><img src='/video_feed'>"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
